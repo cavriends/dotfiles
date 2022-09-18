be6b3919a8b94d41ac5fa9243f4632bd9862206b
Corné Vriends <cavriends@gmail.com>
2022-09-18 21:03:14 +0200

    Cleaned up Polybar directory

--- xps-13-9370-debian/.config/polybar/launch.sh
+++ xps-13-9370-debian/.config/polybar/launch.sh
@@ -21,56 +21,8 @@ launch_bar() {
 	fi
 }
 
-if [[ "$1" == "--material" ]]; then
-	style="material"
-	launch_bar
-
-elif [[ "$1" == "--shades" ]]; then
-	style="shades"
-	launch_bar
-
-elif [[ "$1" == "--hack" ]]; then
-	style="hack"
-	launch_bar
-
-elif [[ "$1" == "--docky" ]]; then
-	style="docky"
-	launch_bar
-
-elif [[ "$1" == "--cuts" ]]; then
-	style="cuts"
-	launch_bar
-
-elif [[ "$1" == "--shapes" ]]; then
-	style="shapes"
-	launch_bar
-
-elif [[ "$1" == "--grayblocks" ]]; then
-	style="grayblocks"
-	launch_bar
-
-elif [[ "$1" == "--blocks" ]]; then
-	style="blocks"
-	launch_bar
-
-elif [[ "$1" == "--colorblocks" ]]; then
-	style="colorblocks"
-	launch_bar
-
-elif [[ "$1" == "--forest" ]]; then
-	style="forest"
-	launch_bar
-
-elif [[ "$1" == "--pwidgets" ]]; then
-	style="pwidgets"
-	launch_bar
-
-elif [[ "$1" == "--panels" ]]; then
-	style="panels"
-	launch_bar
-
-elif [[ "$1" == "--nordic"  ]]; then
-	style="nordic-theme" 
+if [[ "$1" == "--nordic-material" ]]; then
+	style="nordic-material"
 	launch_bar
 
 else
@@ -78,8 +30,5 @@ else
 	Usage : launch.sh --theme
 		
 	Available Themes :
-	--blocks    --colorblocks    --cuts      --docky
-	--forest    --grayblocks     --hack      --material
-	--panels    --pwidgets       --shades    --shapes
+	--nordic-material
 	EOF
-fi
